53457c2d81feb2d8c866f7ff3b2a8818a1a6f7ea
Injung Chung <mu29@yeoubi.net>
2019-05-11 09:22:49 +0900

    feat: linear-gradient 라이브러리를 추가한다

--- android/app/src/main/java/com/about/MainApplication.java
+++ android/app/src/main/java/com/about/MainApplication.java
@@ -3,6 +3,7 @@ package com.about;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.BV.LinearGradient.LinearGradientPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new LinearGradientPackage(),
             new VectorIconsPackage(),
             new RNGestureHandlerPackage()
       );
