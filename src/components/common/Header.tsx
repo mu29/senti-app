9a8653c84e16d1612cf3d5f3232e33a1eda170c9
Injung Chung <mu29@yeoubi.net>
2019-06-04 17:53:59 +0900

    feat: Header 컴포넌트에 뒤로가기 버튼을 추가한다

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,40 +1,79 @@
 import React from 'react';
 import {
   View,
+  Image,
   StyleSheet,
-  Platform,
 } from 'react-native';
-import { Text } from 'components';
+import {
+  withNavigation,
+  NavigationInjectedProps,
+} from 'react-navigation';
+import {
+  Text,
+  Button,
+} from 'components';
 import {
   palette,
   typography,
 } from 'constants/style';
 
+const BACK_ICON = { uri: 'ic_back' };
+
 export interface HeaderProps {
   children: React.ReactNode;
+  canGoBack?: boolean;
 }
 
-const Header: React.FunctionComponent<HeaderProps> = ({
-  children,
-}) => (
-  <View style={styles.container}>
-    <Text style={typography.heading1}>
-      {children}
-    </Text>
-  </View>
-);
+class Header extends React.PureComponent<HeaderProps & NavigationInjectedProps> {
+  public render() {
+    const {
+      canGoBack,
+      children,
+    } = this.props;
+
+    return (
+      <View style={styles.container}>
+        {canGoBack && (
+          <Button onPress={this.goBack} round>
+            <Image style={styles.icon} source={BACK_ICON} />
+          </Button>
+        )}
+        <View pointerEvents="box-none" style={styles.title}>
+          <Text style={typography.heading1}>
+            {children}
+          </Text>
+        </View>
+      </View>
+    );
+  }
+
+  private goBack = () => {
+    this.props.navigation.goBack();
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
-    justifyContent: 'center',
+    height: 48,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
-    padding: Platform.select({
-      ios: 12,
-      android: 15,
-    }),
+    paddingHorizontal: 12,
     borderBottomColor: palette.gray[80],
     borderBottomWidth: 0.5,
   },
+  title: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  icon: {
+    width: 16,
+    height: 16,
+    tintColor: palette.gray[20],
+  },
 });
 
-export default React.memo(Header);
+export default withNavigation(Header);
