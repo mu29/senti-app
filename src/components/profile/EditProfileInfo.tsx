8e81a3bddeae84b6819f4d1dd99076fd70d79971
Injung Chung <mu29@yeoubi.net>
2019-06-25 17:34:42 +0900

    wip: 프로필 편집 화면 아이콘을 배치한다

--- src/components/profile/EditProfileInfo.tsx
+++ src/components/profile/EditProfileInfo.tsx
@@ -1,1 +1,28 @@
 import React from 'react';
+import {
+  View,
+  TextInput,
+  StyleSheet,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { palette } from 'constants/style';
+
+class EditProfileInfo extends React.Component {
+  public render() {
+    return (
+      <View style={styles.form}>
+        <Icon name="md-person" size={18} color={palette.gray[40]} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  form: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginHorizontal: 16,
+  },
+});
+
+export default EditProfileInfo;
