0fc2dd17951da595fd00b00cf62d5eb43277b612
Injung Chung <mu29@yeoubi.net>
2019-06-26 14:09:28 +0900

    feat: EditProfileInfo 컴포넌트를 추가한다

--- src/components/profile/EditProfileInfo.tsx
+++ src/components/profile/EditProfileInfo.tsx
@@ -4,24 +4,134 @@ import {
   TextInput,
   StyleSheet,
 } from 'react-native';
+import {
+  inject,
+  observer,
+} from 'mobx-react/native';
 import Icon from 'react-native-vector-icons/Ionicons';
-import { palette } from 'constants/style';
+import { Text } from 'components';
+import { AuthState } from 'stores/states';
+import { palette, typography } from 'constants/style';
 
-class EditProfileInfo extends React.Component {
+interface EditProfileInfoProps {
+  authState?: AuthState;
+}
+
+@inject('authState')
+@observer
+class EditProfileInfo extends React.Component<EditProfileInfoProps> {
   public render() {
+    const { user } = this.props.authState!;
+
+    if (!user) {
+      return null;
+    }
+
     return (
-      <View style={styles.form}>
-        <Icon name="md-person" size={18} color={palette.gray[40]} />
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={[typography.tiny3, styles.title]}>
+            계정
+          </Text>
+        </View>
+        <View style={styles.form}>
+          <Icon name="md-contact" size={20} color={palette.gray[60]} />
+          <TextInput
+            placeholder="닉네임"
+            placeholderTextColor={palette.gray[50]}
+            spellCheck={false}
+            autoCorrect={false}
+            autoCapitalize="none"
+            dataDetectorTypes="none"
+            keyboardType="default"
+            underlineColorAndroid="transparent"
+            multiline={false}
+            maxLength={40}
+            style={styles.input}
+          >
+            {user.name}
+          </TextInput>
+        </View>
+        <View style={styles.form}>
+          <Icon name="md-heart" size={20} color={palette.gray[60]} />
+          <TextInput
+            placeholder="성별"
+            placeholderTextColor={palette.gray[50]}
+            spellCheck={false}
+            autoCorrect={false}
+            autoCapitalize="none"
+            dataDetectorTypes="none"
+            keyboardType="default"
+            underlineColorAndroid="transparent"
+            multiline={false}
+            maxLength={40}
+            style={styles.input}
+          />
+        </View>
+        <View style={styles.form}>
+          <Icon name="md-exit" size={20} color={palette.gray[60]} />
+          <Text style={styles.text}>
+            로그아웃
+          </Text>
+        </View>
+        <View style={styles.header}>
+          <Text style={[typography.tiny3, styles.title]}>
+            지원
+          </Text>
+        </View>
+        <View style={styles.form}>
+          <Icon name="ios-mail" size={20} color={palette.gray[60]} />
+          <Text style={styles.text}>
+            건의 및 불편 신고
+          </Text>
+        </View>
+        <View style={styles.form}>
+          <Icon name="ios-filing" size={20} color={palette.gray[60]} />
+          <Text style={styles.text}>
+            이용약관 및 개인정보처리방침
+          </Text>
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    paddingVertical: 14,
+    marginHorizontal: 16,
+    marginBottom: 8,
+    borderBottomWidth: 1,
+    borderBottomColor: palette.gray[90],
+  },
+  title: {
+    fontWeight: '600',
+  },
   form: {
     flexDirection: 'row',
     alignItems: 'center',
     marginHorizontal: 16,
+  },
+  input: {
+    flex: 1,
+    marginLeft: 14,
+    paddingVertical: 14,
+    fontSize: 16,
+    color: palette.gray[10],
+    // borderBottomColor: palette.gray[90],
+    // borderBottomWidth: 0.5,
+  },
+  text: {
+    flex: 1,
+    marginLeft: 14,
+    paddingVertical: 14,
+    fontSize: 16,
+    color: palette.gray[10],
+    // borderTopColor: palette.gray[90],
+    // borderTopWidth: 0.5,
   },
 });
 
--- src/screens/EditProfileScreen.tsx
+++ src/screens/EditProfileScreen.tsx
@@ -1,17 +1,50 @@
 import React from 'react';
 import {
+  ScrollView,
+  StyleSheet,
+} from 'react-native';
+import {
+  Button,
+  Text,
   Header,
+  EditProfileInfo,
   EditProfilePhoto,
   withSafeArea,
 } from 'components';
+import { palette } from 'constants/style';
+
+const HIT_SLOP = {
+  top: 16,
+  left: 16,
+  right: 16,
+  bottom: 16,
+};
 
 const EditProfileScreen = () => (
   <React.Fragment>
     <Header canGoBack>
-      프로필 편집
+      정보 관리
+      <Button hitSlop={HIT_SLOP} style={styles.button}>
+        <Text style={styles.text}>
+           저장
+        </Text>
+      </Button>
     </Header>
-    <EditProfilePhoto />
+    <ScrollView>
+      <EditProfilePhoto />
+      <EditProfileInfo />
+    </ScrollView>
   </React.Fragment>
 );
+
+const styles = StyleSheet.create({
+  button: {
+    marginRight: 4,
+  },
+  text: {
+    color: palette.yellow.default,
+    fontSize: 16,
+  },
+});
 
 export default withSafeArea(EditProfileScreen);
