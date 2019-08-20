75e7af1793ff417166fa4564934dcfbbcaa68975
Injung Chung <mu29@yeoubi.net>
2019-08-20 14:58:53 +0900

    feat: useAudio 훅을 추가한다

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,2 +1,3 @@
 export * from './AudioContext';
 export { default as SoundService } from './SoundService';
+export { default as useAudio } from './useAudio';
--- src/services/useAudio.tsx
+++ src/services/useAudio.tsx
@@ -1,19 +1,81 @@
 import {
   useState,
-  useRef,
+  useCallback,
+  useEffect,
 } from 'react';
-import Sound from 'react-native-sound';
+import SoundService, { SoundState } from './SoundService';
 
 interface Audio {
-  url: string;
-  sound?: Sound;
   elapsedTime: number;
+  isLoading: boolean;
+  isActivated: boolean;
   isPlaying: boolean;
 }
 
-let timer: NodeJS.Timer;
+function useAudio(key: string) {
+  const [audio, setAudio] = useState<Audio>({
+    elapsedTime: 0,
+    isLoading: false,
+    isActivated: false,
+    isPlaying: false,
+  });
 
-function useAudio() {
-  const current = useRef('');
-  const [audios, setAudios] = useState<{ [key: string]: Audio }>({});
+  const soundObserver = useCallback((state: SoundState) => {
+    switch (state) {
+      case SoundState.PLAY:
+        setAudio((prev) => ({
+          ...prev,
+          isLoading: false,
+          isActivated: true,
+          isPlaying: true,
+        }));
+        break;
+      case SoundState.PAUSE:
+        setAudio((prev) => ({
+          ...prev,
+          isPlaying: false,
+        }));
+        break;
+      case SoundState.STOP:
+        setAudio((prev) => ({
+          ...prev,
+          elapsedTime: 0,
+          isPlaying: false,
+        }));
+        break;
+      case SoundState.LOADING:
+        setAudio((prev) => ({
+          ...prev,
+          isLoading: true,
+        }));
+        break;
+      case SoundState.ERROR:
+        SoundService.play(key);
+        break;
+      case SoundState.NONE:
+        setAudio({
+          elapsedTime: 0,
+          isLoading: false,
+          isActivated: false,
+          isPlaying: false,
+        });
+        break;
+    }
+  }, [key]);
+
+  useEffect(() => {
+    SoundService.addObserver(key, soundObserver);
+
+    return SoundService.removeObserver(key, soundObserver);
+  }, []);
+
+  return {
+    audio,
+    play: SoundService.play,
+    pause: SoundService.pause,
+    replay: SoundService.replay,
+    release: SoundService.release,
+  };
 }
+
+export default useAudio;
