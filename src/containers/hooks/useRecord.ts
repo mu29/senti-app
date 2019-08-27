5d00c2fbe460f546404967cf5b5e05305c3bdec9
Injung Chung <mu29@yeoubi.net>
2019-08-27 14:19:35 +0900

    fix: useRecord 훅이 애니메이션이 끝나기를 기다리도록 수정한다

--- src/containers/hooks/useRecord.ts
+++ src/containers/hooks/useRecord.ts
@@ -1,4 +1,8 @@
 import { useState, useCallback } from 'react';
+import {
+  Alert,
+  InteractionManager,
+} from 'react-native';
 import { RecordService } from 'services';
 
 function useRecord() {
@@ -6,26 +10,40 @@ function useRecord() {
   const [isRecorded, setIsRecorded] = useState(false);
   const [isStarted, setIsStarted] = useState(false);
 
+  const start = useCallback(() => {
+    setIsStarted(true);
+    if (isRecorded) {
+      RecordService.play(() => setIsStarted(false));
+    } else {
+      requestAnimationFrame(() => {
+        RecordService.start();
+      });
+    }
+  }, [isRecorded]);
+
+  const stop = useCallback(() => {
+    if (isRecorded) {
+      RecordService.pause();
+    } else {
+      InteractionManager.runAfterInteractions(() => {
+        RecordService.stop()
+          .then((result) => {
+            setData(result);
+            setIsRecorded(true);
+          })
+          .catch(() => Alert.alert('알림', '녹음에 실패했습니다. 다시 시도해 주세요.'));
+      });
+    }
+    setIsStarted(false);
+  }, [isRecorded]);
+
   const toggle = useCallback(() => {
     if (isStarted) {
-      if (isRecorded) {
-        RecordService.pause();
-      } else {
-        RecordService.stop().then((result) => {
-          setData(result);
-          setIsRecorded(true);
-        });
-      }
-      setIsStarted(false);
+      stop();
     } else {
-      setIsStarted(true);
-      if (isRecorded) {
-        RecordService.play(RecordService.pause);
-      } else {
-        RecordService.start();
-      }
+      start();
     }
-  }, [isStarted, isRecorded]);
+  }, [start, stop, isStarted]);
 
   const release = useCallback(() => {
     RecordService.release();
@@ -33,11 +51,11 @@ function useRecord() {
   }, []);
 
   return {
+    data,
     isRecorded,
     isStarted,
     toggle,
     release,
-    data,
   };
 }
 
