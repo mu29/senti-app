eea6c65121880d99f9e8dd7ed1d309061ff22d37
Injung Chung <mu29@yeoubi.net>
2019-09-01 09:53:19 +0900

    feat: 오디오 재생 시간을 업데이트한다

--- src/containers/hooks/useAudio.ts
+++ src/containers/hooks/useAudio.ts
@@ -2,6 +2,7 @@ import {
   useState,
   useCallback,
   useEffect,
+  useRef,
 } from 'react';
 import {
   AudioService,
@@ -23,39 +24,54 @@ function useAudio(key: string) {
     isPlaying: false,
   });
 
+  const timer = useRef<NodeJS.Timeout>();
+
   const soundObserver = useCallback((state: AudioState) => {
     switch (state) {
       case AudioState.PLAY:
-        setAudio((prev) => ({
+        setAudio(prev => ({
           ...prev,
           isLoading: false,
           isActivated: true,
           isPlaying: true,
         }));
+        timer.current = setInterval(() => setAudio(prev => ({
+          ...prev,
+          elapsedTime: prev.elapsedTime + 500,
+        })), 500);
         break;
       case AudioState.PAUSE:
-        setAudio((prev) => ({
+        if (timer.current) {
+          clearInterval(timer.current);
+        }
+        setAudio(prev => ({
           ...prev,
           isPlaying: false,
         }));
         break;
       case AudioState.STOP:
-        setAudio((prev) => ({
+        if (timer.current) {
+          clearInterval(timer.current);
+        }
+        setAudio(prev => ({
           ...prev,
           elapsedTime: 0,
           isPlaying: false,
         }));
         break;
       case AudioState.LOADING:
-        setAudio((prev) => ({
+        setAudio(prev => ({
           ...prev,
           isLoading: true,
         }));
         break;
       case AudioState.ERROR:
-        AudioService.play(key);
+        setTimeout(() => AudioService.play(key), 1000);
         break;
       case AudioState.NONE:
+        if (timer.current) {
+          clearInterval(timer.current);
+        }
         setAudio({
           elapsedTime: 0,
           isLoading: false,
