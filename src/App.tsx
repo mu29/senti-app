1737387da730cda47f4c32891354c5360f804db3
Injung Chung <mu29@yeoubi.net>
2019-09-05 10:20:32 +0900

    feat: 클라이언트 캐시 기능을 추가한다

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
+import ApolloClient from 'apollo-client';
+import { NormalizedCacheObject } from 'apollo-cache-inmemory';
 import { ApolloProvider } from '@apollo/react-hooks';
-import firebase from 'react-native-firebase';
+import firebase, { RNFirebase } from 'react-native-firebase';
 import Sound from 'react-native-sound';
 import moment from 'moment';
 import 'moment/locale/ko';
@@ -9,7 +11,7 @@ import { AuthModal } from 'containers';
 import { FETCH_PROFILE } from './graphqls';
 import Navigator from './Navigator';
 import NavigationService from './NavigationService';
-import client from './apollo';
+import configureClient from './apollo';
 
 // @ts-ignore
 Sound.enableInSilenceMode(true);
@@ -18,25 +20,52 @@ Sound.setActive(true);
 moment.locale('ko');
 
 interface State {
+  user: RNFirebase.User | null;
+  client?: ApolloClient<NormalizedCacheObject>;
+  hasCacheLoaded: boolean;
   hasAuthTriggered: boolean;
 }
 
-class App extends React.Component<{}, State> {
-  public state = {
+class App extends React.PureComponent<{}, State> {
+  public state: State = {
+    user: null,
+    client: undefined,
+    hasCacheLoaded: false,
     hasAuthTriggered: false,
   };
+
+  private isReady = false;
 
   private authStateUnsubscriber?: () => void;
 
   constructor(props: {}) {
     super(props);
-    this.authStateUnsubscriber = firebase.auth().onAuthStateChanged(() => {
+    this.authStateUnsubscriber = firebase.auth().onAuthStateChanged((user) => {
+      if (this.isReady) {
+        this.setState({ user });
+      } else {
+        this.state = Object.assign(this.state, { user });
+      }
+    });
+  }
+
+  public async componentDidMount() {
+    this.isReady = true;
+    this.setState({
+      client: await configureClient(),
+      hasCacheLoaded: true,
+    });
+  }
+
+  public componentDidUpdate() {
+    const { client } = this.state;
+    if (client) {
       client.query({
         query: FETCH_PROFILE,
         fetchPolicy: 'network-only',
       })
       .finally(() => this.setState({ hasAuthTriggered: true }));
-    });
+    }
   }
 
   public componentWillUnmount() {
@@ -46,9 +75,13 @@ class App extends React.Component<{}, State> {
   }
 
   public render() {
-    const { hasAuthTriggered } = this.state;
+    const {
+      client,
+      hasCacheLoaded,
+      hasAuthTriggered,
+    } = this.state;
 
-    if (!hasAuthTriggered) {
+    if (!client || !hasCacheLoaded || !hasAuthTriggered) {
       return null;
     }
 
--- src/apollo.ts
+++ src/apollo.ts
@@ -1,90 +1,103 @@
+import AsyncStorage from '@react-native-community/async-storage';
 import firebase from 'react-native-firebase';
 import merge from 'lodash/merge';
 import ApolloClient from 'apollo-client';
 import { BatchHttpLink } from 'apollo-link-batch-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { persistCache } from 'apollo-cache-persist';
 import gql from 'graphql-tag';
 import covers from 'constants/covers';
 import { getLanguage } from 'utils';
 import * as resolvers from './resolvers';
 import config from './config';
 
-const customFetch = async (uri: RequestInfo, options?: RequestInit) => {
-  const { currentUser } = firebase.auth();
-  if (options && options.headers && currentUser) {
-    (options.headers as any).Authorization = await currentUser.getIdToken();
-  }
-
-  return fetch(uri, options);
-};
-
-const link = new BatchHttpLink({
-  uri: `${config.apiUrl}?language=${getLanguage()}`,
-  fetch: customFetch,
-});
-
-const client = new ApolloClient({
-  link,
-  cache: new InMemoryCache(),
-  resolvers: Object.values(resolvers).reduce((c, r) => merge(c, r), {}),
-  typeDefs: gql`
-    type Modal {
-      id: ID!
-      params: String
-      isVisible: Boolean!
+export async function configureClient() {
+  const customFetch = async (uri: RequestInfo, options?: RequestInit) => {
+    const { currentUser } = firebase.auth();
+    if (options && options.headers && currentUser) {
+      (options.headers as any).Authorization = await currentUser.getIdToken();
     }
 
-    extend type Query {
-      modal(id: ID!): Modal
-      searchQuery: String
-    }
+    return fetch(uri, options);
+  };
 
-    extend type Mutation {
-      showModal(id: ID!): Boolean
-      hideModal(id: ID!): Boolean
-      shuffleCovers: Boolean
-    }
-  `,
-});
+  const link = new BatchHttpLink({
+    uri: `${config.apiUrl}?language=${getLanguage()}`,
+    fetch: customFetch,
+  });
 
-const initialData = {
-  data: {
-    modals: [{
-      __typename: 'Modal',
-      id: 'Auth',
-      params: null,
-      isVisible: false,
-    }, {
-      __typename: 'Modal',
-      id: 'Reply',
-      params: null,
-      isVisible: false,
-    }, {
-      __typename: 'Modal',
-      id: 'Cover',
-      params: null,
-      isVisible: false,
-    }],
-    searchQuery: '',
-    covers,
-    draft: {
-      __typename: 'Draft',
-      cover: '',
-      message: '',
+  const cache = new InMemoryCache();
+
+  await persistCache({
+    cache,
+    storage: AsyncStorage as any,
+  });
+
+  const client = new ApolloClient({
+    link,
+    cache,
+    resolvers: Object.values(resolvers).reduce((c, r) => merge(c, r), {}),
+    typeDefs: gql`
+      type Modal {
+        id: ID!
+        params: String
+        isVisible: Boolean!
+      }
+
+      extend type Query {
+        modal(id: ID!): Modal
+        searchQuery: String
+      }
+
+      extend type Mutation {
+        showModal(id: ID!): Boolean
+        hideModal(id: ID!): Boolean
+        shuffleCovers: Boolean
+      }
+    `,
+  });
+
+  const initialData = {
+    data: {
+      modals: [{
+        __typename: 'Modal',
+        id: 'Auth',
+        params: null,
+        isVisible: false,
+      }, {
+        __typename: 'Modal',
+        id: 'Reply',
+        params: null,
+        isVisible: false,
+      }, {
+        __typename: 'Modal',
+        id: 'Cover',
+        params: null,
+        isVisible: false,
+      }],
+      searchQuery: '',
+      covers,
+      draft: {
+        __typename: 'Draft',
+        cover: '',
+        message: '',
+      },
+      candidate: {
+        __typename: 'Candidate',
+        name: null,
+        gender: null,
+      },
     },
-    candidate: {
-      __typename: 'Candidate',
-      name: null,
-      gender: null,
-    },
-  },
-};
+  };
 
-client.cache.writeData(initialData);
-
-client.onResetStore(() => {
   client.cache.writeData(initialData);
-  return Promise.resolve();
-});
 
-export default client;
+  client.onResetStore(() => {
+    client.cache.writeData(initialData);
+    return Promise.resolve();
+  });
+
+  return client;
+}
+
+export default configureClient;
