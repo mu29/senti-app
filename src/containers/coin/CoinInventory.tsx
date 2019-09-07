4d3050fa84528fbf4dae3015f81e754b9c958b6c
Injung Chung <mu29@yeoubi.net>
2019-09-07 17:13:54 +0900

    fix: 프로필에 현재 보유 코인을 표시한다

--- src/containers/coin/CoinInventory.tsx
+++ src/containers/coin/CoinInventory.tsx
@@ -1,16 +1,32 @@
 import React from 'react';
-import { useMutation } from '@apollo/react-hooks';
+import firebase from 'react-native-firebase';
+import {
+  useQuery,
+  useMutation,
+} from '@apollo/react-hooks';
 import { CoinInventory } from 'components';
-import { SHOW_MODAL } from 'graphqls';
+import {
+  FETCH_PROFILE,
+  SHOW_MODAL,
+} from 'graphqls';
 
 const Container: React.FunctionComponent<{}> = () => {
+  const { data } = useQuery<{ me: Profile }>(FETCH_PROFILE, {
+    skip: !firebase.auth().currentUser,
+    fetchPolicy: 'cache-only',
+  });
+
   const [showModal] = useMutation(SHOW_MODAL, {
     variables: { id: 'Coin' },
   });
 
+  if (!data || !data.me) {
+    return null;
+  }
+
   return (
     <CoinInventory
-      amount={16}
+      amount={data.me.coin || 0}
       showModal={showModal}
     />
   );
