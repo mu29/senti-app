82609cc52e49351c6f4ef376ea18867798f8ab39
Injung Chung <mu29@yeoubi.net>
2019-09-19 01:12:02 +0900

    style: 앱 아이콘과 스플래시 화면을 변경한다

--- ios/Senti/AppDelegate.m
+++ ios/Senti/AppDelegate.m
@@ -30,7 +30,7 @@
                                                    moduleName:@"Senti"
                                             initialProperties:nil];
 
-  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+  rootView.backgroundColor = [[UIColor alloc] initWithRed:0.102f green:0.109f blue:0.161f alpha:1];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
