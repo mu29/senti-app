5ce5167baa4d71f5334e417376a80a3653dc941a
Injung Chung <mu29@yeoubi.net>
2019-09-18 16:22:14 +0900

    pref: React Native 버전을 올리고 안드로이드 설정을 수정한다

--- android/app/src/main/java/com/senti/app/MainApplication.java
+++ android/app/src/main/java/com/senti/app/MainApplication.java
@@ -1,37 +1,24 @@
 package com.senti.app;
 
 import android.app.Application;
+import android.util.Log;
 
+import com.RNFetchBlob.RNFetchBlobPackage;
+import com.facebook.react.PackageList;
 import com.facebook.CallbackManager;
 import com.facebook.react.ReactApplication;
-import com.microsoft.codepush.react.CodePush;
-import org.devio.rn.splashscreen.SplashScreenReactPackage;
-import net.yeoubi.iap.RNInAppPurchasePackage;
-import com.swmansion.reanimated.ReanimatedPackage;
-import com.reactnativecommunity.viewpager.RNCViewPagerPackage;
-import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
-import com.taluttasgiran.actionsheet.RNActionsheetPackage;
-import com.reactnative.ivpusic.imagepicker.PickerPackage;
-import com.RNFetchBlob.RNFetchBlobPackage;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
-import co.apptailor.googlesignin.RNGoogleSigninPackage;
-import io.invertase.firebase.RNFirebasePackage;
-import io.invertase.firebase.analytics.RNFirebaseAnalyticsPackage;
-import io.invertase.firebase.auth.RNFirebaseAuthPackage;
-import io.invertase.firebase.storage.RNFirebaseStoragePackage;
-import io.invertase.firebase.fabric.crashlytics.RNFirebaseCrashlyticsPackage;
-import com.zmxv.RNSound.RNSoundPackage;
-import com.kevinresol.react_native_sound_recorder.RNSoundRecorderPackage;
-import com.rnfs.RNFSPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+import com.microsoft.codepush.react.CodePush;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
-import java.util.Arrays;
 import java.util.List;
+
+import io.invertase.firebase.analytics.RNFirebaseAnalyticsPackage;
+import io.invertase.firebase.auth.RNFirebaseAuthPackage;
+import io.invertase.firebase.fabric.crashlytics.RNFirebaseCrashlyticsPackage;
+import io.invertase.firebase.storage.RNFirebaseStoragePackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -50,31 +37,20 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new CodePush(BuildConfig.CODEPUSH_KEY, getApplicationContext(), BuildConfig.DEBUG),
-            new SplashScreenReactPackage(),
-            new RNInAppPurchasePackage(),
-            new ReanimatedPackage(),
-            new RNCViewPagerPackage(),
-            new AsyncStoragePackage(),
-            new RNActionsheetPackage(),
-            new PickerPackage(),
-            new RNFetchBlobPackage(),
-            new FBSDKPackage(mCallbackManager),
-            new RNGoogleSigninPackage(),
-            new RNFirebaseAnalyticsPackage(),
-            new RNFirebasePackage(),
-            new RNFirebaseAuthPackage(),
-            new RNFirebaseStoragePackage(),
-            new RNFirebaseCrashlyticsPackage(),
-            new RNSoundPackage(),
-            new RNSoundRecorderPackage(),
-            new RNFSPackage(),
-            new VectorIconsPackage(),
-            new RNGestureHandlerPackage(),
-            new RNConfigPackage()
-      );
+      List<ReactPackage> packages = new PackageList(this).getPackages();
+      packages.add(new CodePush(BuildConfig.CODEPUSH_KEY, getApplicationContext(), BuildConfig.DEBUG));
+      packages.add(new FBSDKPackage(mCallbackManager));
+      packages.add(new RNFetchBlobPackage());
+      packages.add(new RNFirebaseAnalyticsPackage());
+      packages.add(new RNFirebaseAuthPackage());
+      try {
+        packages.add(new RNFirebaseStoragePackage());
+      } catch (SecurityException e) {
+        Log.e("Senti:RNFirebaseStorage",  e.getMessage());
+      }
+      packages.add(new RNFirebaseCrashlyticsPackage());
+      packages.add(new RNConfigPackage());
+      return packages;
     }
 
     @Override
