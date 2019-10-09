1d3bef1cb97214360fd69bbd47ea5dbf13a7d186
Injung Chung <mu29@yeoubi.net>
2019-10-09 16:41:23 +0900

    fix: 새 이야기를 생성하면 메인 피드와 나의 피드에 각각 추가한다

--- src/containers/story/CreateStoryController.tsx
+++ src/containers/story/CreateStoryController.tsx
@@ -15,15 +15,73 @@ import { RecordController } from 'containers';
 import {
   FETCH_DRAFT,
   CREATE_STORY,
+  FETCH_MAIN_STORY_FEED,
+  FETCH_MY_STORY_FEED,
 } from 'graphqls';
 import { AnalyticsService } from 'services';
+
+type MainStoryFeedResult = {
+  mainStoryFeed: {
+    stories: Story[];
+    cursor: string;
+  };
+};
+
+type MyStoryFeedResult = {
+  myStoryFeed: {
+    stories: Story[];
+    cursor: string;
+  };
+};
 
 const Container: React.FunctionComponent<NavigationInjectedProps> = ({
   navigation,
 }) => {
   const { data } = useQuery(FETCH_DRAFT);
 
-  const [createStory] = useMutation(CREATE_STORY);
+  const [createStory] = useMutation(CREATE_STORY, {
+    update: (cache, { data: { createStory: story } }) => {
+      try {
+        const savedMainFeed = cache.readQuery<MainStoryFeedResult>({
+          query: FETCH_MAIN_STORY_FEED,
+        });
+
+        if (!savedMainFeed) {
+          return;
+        }
+
+        cache.writeQuery({
+          query: FETCH_MAIN_STORY_FEED,
+          data: {
+            mainStoryFeed: {
+              ...savedMainFeed.mainStoryFeed,
+              stories: [story, ...savedMainFeed.mainStoryFeed.stories],
+            },
+          },
+        });
+      } catch {}
+
+      try {
+        const savedMyFeed = cache.readQuery<MyStoryFeedResult>({
+          query: FETCH_MY_STORY_FEED,
+        });
+
+        if (!savedMyFeed) {
+          return;
+        }
+
+        cache.writeQuery({
+          query: FETCH_MY_STORY_FEED,
+          data: {
+            myStoryFeed: {
+              ...savedMyFeed.myStoryFeed,
+              stories: [story, ...savedMyFeed.myStoryFeed.stories],
+            },
+          },
+        });
+      } catch {}
+    }
+  });
 
   const [isLoading, setIsLoading] = useState(false);
 
