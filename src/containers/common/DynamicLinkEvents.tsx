50b37d8c66cef1de62d9951e3b887d86deb3f4ac
Injung Chung <mu29@yeoubi.net>
2019-10-19 20:06:29 +0900

    fix: DynamicLinkEvents 컴포넌트에서 다이나믹 링크를 처리하던 방법을 변경한다

--- src/containers/common/DynamicLinkEvents.tsx
+++ src/containers/common/DynamicLinkEvents.tsx
@@ -1,26 +1,57 @@
-import React, { useEffect } from 'react';
+import React, {
+  useMemo,
+  useEffect,
+  useCallback,
+} from 'react';
 import firebase from 'react-native-firebase';
+import AsyncStorage from '@react-native-community/async-storage';
+import { useMutation } from '@apollo/react-hooks';
+import { SHOW_MODAL } from 'graphqls';
 import { WEBSITE_URL } from 'constants/config';
+import { Alert } from 'react-native';
 
 const DynamicLinkEvents: React.FunctionComponent<{}> = () => {
+  const [showAuthModal] = useMutation(SHOW_MODAL, {
+    variables: { id: 'Auth' },
+  });
+
+  const handlers = useMemo(() => [{
+    regex: /\/referral\??(.*)/,
+    handle: (params: Record<string, any>) => {
+      AsyncStorage.setItem('@Referral', params.id)
+        .then(() => {
+          Alert.alert('오우', params.id)
+          showAuthModal()
+        })
+        .catch(console.error);
+    }
+  }], [showAuthModal]);
+
+  const handleDynamicLink = useCallback((url: string | null) => {
+    if (!url) {
+      return;
+    }
+
+    const path = url.replace(WEBSITE_URL, '');
+
+    handlers.forEach((handler) => {
+      const matched = path.match(handler.regex);
+
+      if (matched) {
+        const params = JSON.parse(matched[1] || '{}');
+        handler.handle(params);
+      }
+    });
+  }, [handlers]);
+
   useEffect(() => {
-    firebase.links()
-      .getInitialLink()
-      .then((url) => {
-        if (!url) {
-          return;
-        }
+    firebase.links().getInitialLink().then(handleDynamicLink).catch(console.error);
+  }, [handleDynamicLink]);
 
-        const path = url.replace(WEBSITE_URL, '');
-
-        switch (path) {
-          case '/earlybird':
-            break;
-          case '/referral':
-            break;
-        }
-      });
-  }, []);
+  useEffect(() => {
+    const unsubscriber = firebase.links().onLink(handleDynamicLink);
+    return () => unsubscriber();
+  }, [handleDynamicLink]);
 
   return null;
 };
