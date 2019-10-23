cb5f07731b88b9dd457635b20f0bf31806b8d552
Injung Chung <mu29@yeoubi.net>
2019-10-23 12:51:46 +0900

    fix: 다이나믹 링크 생성 시 앱 스토어 ID를 추가한다

--- src/services/DynamicLinkService.ts
+++ src/services/DynamicLinkService.ts
@@ -11,6 +11,7 @@ class DynamicLinkService {
     const link = new firebase.links.DynamicLink(`https://${DYNAMIC_LINK_HOST}/referral?id=${userId}`, `https://${DYNAMIC_LINK_HOST}`)
       .android.setPackageName(PACKAGE_NAME)
       .ios.setBundleId(PACKAGE_NAME)
+      .ios.setAppStoreId('1483156767')
       .social.setTitle(LocalizedStrings.COMMON_APP_NAME)
       .social.setDescriptionText(LocalizedStrings.COMMON_APP_DESCRIPTION)
       .social.setImageUrl(`${WEBSITE_URL}/assets/opengraph.png`);
